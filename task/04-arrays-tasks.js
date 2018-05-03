140a8fda0a991af1bc7b6f7bf0b5bfc7d4af6700
Eugene <eugenevaleska1994@gmail.com>
2018-05-03 15:41:50 +0300

    Done. All tests passed.

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -527,7 +527,10 @@ function getIntervalArray(start, end) {
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
 function distinct(arr) {
-   throw new Error('Not implemented');
+  return arr.filter(onlyUnique);
+}
+function onlyUnique(value, index, array) {
+    return array.indexOf(value) === index;
 }
 
 /**
